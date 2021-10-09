44c8419c50fb66c12b8f75e28cbaf01b79fe8739
chandrabhushann <chandrabhushan.nulu@gmail.com>
2021-10-09 09:26:42 +0530

    initial commit of basic flask server

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hellp world"
+
+@app.route('/chandra')
+def chandra():
+    return "hellp chandra"
+
+if __name__ == '__main__':
+    app.run(debug=True)
